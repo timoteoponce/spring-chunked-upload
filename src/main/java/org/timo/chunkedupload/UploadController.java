ce6a024dbfe5d4b159c04c8cd3089a3848b9e0d6
Timoteo Ponce <timo.slack@gmail.com>
2017-11-21 16:52:18 -0400

    Added behavior to store files locally

--- src/main/java/org/timo/chunkedupload/UploadController.java
+++ src/main/java/org/timo/chunkedupload/UploadController.java
@@ -1,61 +1,116 @@
 package org.timo.chunkedupload;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.Map;
+import org.apache.commons.io.*;
+import org.slf4j.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.*;
 
-import org.apache.commons.io.IOUtils;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.*;
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
 
 /**
  * @author timoteo
- * 
+ * <p>
  * https://github.com/blueimp/jQuery-File-Upload/wiki/Chunked-file-uploads
- * 
- *
  */
 @RestController
 public class UploadController {
 
-	@RequestMapping("/hello")
-	public String sayHi() {
-		return "Hi at " + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
-	}
+    private String filePath = System.getProperty("user.home") + "/tmp/";
 
-	@ResponseBody
-	@RequestMapping(value = "/chunked-upload", method = RequestMethod.POST)
-	public Map<String, String> chunkedUpload(final @RequestHeader HttpHeaders headers,
-			final MultipartHttpServletRequest request) throws IOException {
-		info("Headers are: " + headers.toString());
-		info("File is: " + headers.getFirst("Content-Disposition"));
+    @RequestMapping("/hello")
+    public String sayHi() {
+        return "Hi at " + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+    }
 
-		// MultipartHttpServletRequest multipartRequest =
-		// (MultipartHttpServletRequest) request;
-		request.getFileNames().forEachRemaining(t -> {
-			MultipartFile file = request.getFile(t);
-			info("multipart: " + t);
-			info("multipart-file: " + file.getSize());
-			try {
-				info("multipart-data: " + IOUtils.toByteArray(file.getInputStream()).length);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
-		return Collections.singletonMap("Test", "Value");
-	}
+    @ResponseBody
+    @RequestMapping(value = "/chunked-upload", method = RequestMethod.POST)
+    public Map<String, String> chunkedUpload(final @RequestHeader HttpHeaders headers, final MultipartHttpServletRequest request) {
+        String fileName = extractFilename(headers.getFirst("Content-Disposition"));
+        RangeData range = extractRangeData(headers.getFirst("Content-Range"));
 
-	private void info(String string) {
-		LoggerFactory.getLogger(getClass()).info(string);
-	}
+        File file = new File(filePath + fileName);
+        if (!file.getParentFile().exists() && !file.getParentFile().mkdirs())
+            throw new IllegalStateException("Can't create storage folder: " + file.getAbsolutePath());
+
+        if (range.begin == 0L) {
+            file.delete();
+            
+        } else if (file.length() >= range.totalSize)
+            throw new IllegalStateException("File upload offset: " + range + ", current size: " + file.length());
+
+        request.getFileMap().forEach((name, partFile) -> {
+
+            try (FileOutputStream fos = new FileOutputStream(file, true)) {
+                fos.write(IOUtils.toByteArray(partFile.getInputStream()));
+            } catch (IOException e) {
+                throw new IllegalStateException("Error when processing file content", e);
+            }
+        });
+        return Collections.singletonMap("status", "uploaded");
+    }
+
+    private static String extractFilename(String str) {
+        String key = "filename";
+        int idx = str.indexOf("filename");
+        if (idx >= 0) {
+            String value = str.substring(idx + (key + "=\"").length(), str.length() - 1).trim();
+            try {
+                return URLDecoder.decode(value, StandardCharsets.UTF_8.name());
+            } catch (UnsupportedEncodingException e) {
+                throw new IllegalStateException("Invalid Content-Disposition header content: " + str, e);
+            }
+        }
+        throw new IllegalArgumentException("Invalid Content-Disposition header: " + str);
+    }
+
+    private static RangeData extractRangeData(String str) {
+        String[] parts = str.split("\\W+");
+        if (parts.length > 3)
+            return new RangeData(Long.parseLong(parts[1]), Long.parseLong(parts[2]), Long.parseLong(parts[3]));
+        throw new IllegalArgumentException("Invalid Content-Range header: " + str);
+    }
+
+    private static void info(String string) {
+        LoggerFactory.getLogger(UploadController.class).info(string);
+    }
+
+    private static class RangeData {
+        public final long begin;
+        public final long end;
+        public final long totalSize;
+
+        public RangeData(long begin, long end, long totalSize) {
+            this.begin = begin;
+            this.end = end;
+            this.totalSize = totalSize;
+        }
+
+        @Override
+        public String toString() {
+            return "RangeData{" +
+                    "begin=" + begin +
+                    ", end=" + end +
+                    ", totalSize=" + totalSize +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "attachment; filename=\"presentation_20171005.sql.zip\"";
+        info(str);
+        info(" " + str.indexOf("filename"));
+        info(" " + str.substring(str.indexOf("filename") + "filename=\"".length(), str.length() - 1));
+        info(extractFilename(str));
+
+        String range = "bytes 20000000-24999999/28369757";
+        info(Arrays.toString(range.split("\\W+")));
+        info(extractRangeData(range).toString());
+    }
 
 }
